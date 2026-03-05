7ae2aab5a13500122e8eab27c7858b6de7d82732
Douglas Henrique Pereira Leite <1051570@senacminas.edu.br>
2026-03-05 11:35:20 -0300

    Atividade pontuação

---
+++ pontuacao.py
@@ -0,0 +1,25 @@
+#CRIAÇÃO DE DICIONARIO
+pontos = {}
+
+#ENTRADA DE DADOS
+for i in range(3):
+    nome = input("Digite seu nome: ")  #chave
+    pontuacao = float(input("Informe os pontos iniciais: "))  #valor
+    
+    pontos[nome] = pontuacao
+
+#ATUALIZAÇÃO DE PONTOS
+nome = input("\nDigite o nome do jogador para adicionar pontos: ")
+pontos_adicionados = float(input("Informe os pontos conquistados: "))
+
+#VERIFICAÇÃO E SOMA DE PONTOS
+if nome in pontos:
+    pontos[nome] = pontos[nome] + pontos_adicionados
+    print("Pontos adicionados com sucesso!")
+else:
+    print("Jogador não encontrado.")
+
+#TABELA ATUALIZADA
+print("\nTabela de Pontos Atualizada:")
+for jogador, pontuacao in pontos.items():
+    print(f"{jogador} | {pontuacao}")
